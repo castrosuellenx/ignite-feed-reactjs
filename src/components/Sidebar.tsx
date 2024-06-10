8d6bbab63e6faea8621fece433d7bcb7c06712c3
castrosuellenx <castrosuellenx@gmail.com>
2024-06-10 07:56:47 -0300

    initial commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import {PencilLine} from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/castrosuellenx.png"
+          alt=""
+        />
+
+        <strong>Suéllen Castro</strong>
+        <span>Full Stack Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
