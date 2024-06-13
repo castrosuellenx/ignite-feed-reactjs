8ad1ae5edf5668018c6d59f3a310321c0c68afee
castrosuellenx <castrosuellenx@gmail.com>
2024-06-13 19:09:09 -0300

    add component Comment

---
+++ src/components/Comment.tsx
@@ -0,0 +1,40 @@
+import {ThumbsUp, Trash} from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/castrosuellenx.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Suéllen Castro</strong>
+
+              <time
+                title="09 de Dezembro de 2022"
+                dateTime="2022-09-12 23:09:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
